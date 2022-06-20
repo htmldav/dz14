163c1b5acb8cec502b5e11283c84161fc4df7460
htmldav <84363462+htmldav@users.noreply.github.com>
2022-06-20 15:02:52 +0300

    aws_iam_instance_profile.ec2_profile.name

--- provider.tf
+++ provider.tf
@@ -7,6 +7,8 @@ terraform {
   }
 }
 
+
+
 provider "aws" {
   # shared_config_files      = ["/root/.aws/config"]
   # shared_credentials_file = "/root/.aws/credentials"
@@ -58,6 +60,60 @@ resource "aws_security_group" "instance1606" {
   ]
 }
 
+#Create a policy
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "ec2_policy" {
+  name        = "ec2_policy"
+  path        = "/"
+  description = "Policy to provide permission to EC2"
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+    {
+      Effect = "Allow"
+      Action =["s3:*"]
+      Resource = ["*"]
+    }
+  ]
+  })
+}
+
+#Create a role
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+    {
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com"}
+      Action = "sts:AssumeRole"
+    }
+  ]
+  })
+}
+
+#Attach role to policy
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy_attachment
+resource "aws_iam_policy_attachment" "ec2_policy_role" {
+  name       = "ec2_attachment"
+  roles      = [aws_iam_role.ec2_role.name]
+  policy_arn = aws_iam_policy.ec2_policy.arn
+}
+
+#Attach role to an instance profile
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_instance_profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 resource "aws_key_pair" "generated_key" {
   key_name   = "generated_key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
@@ -68,6 +124,7 @@ resource "aws_instance" "terraforminstance" {
   instance_type               = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance1606.id]
   key_name                    = aws_key_pair.generated_key.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   user_data = "${file("script.sh")}"
  
