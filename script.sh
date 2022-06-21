55c5d0124011dd9c02e0b60c5004d3c095e575c8
htmldav <htmldav@mail.ru>
2022-06-21 05:34:19 +0300

    del sudo mkdir -p /home/ubuntu/dir1806

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y awscli
 sudo apt-get install -y default-jdk
 sudo apt-get install -y maven
 sudo apt-get install -y git
-sudo mkdir -p /home/ubuntu/dir1806 && cd /home/ubuntu/dir1806 && git clone https://github.com/htmldav/boxfuse-sample-java-war-hello.git && cd /home/ubuntu/dir1806/boxfuse-sample-java-war-hello && mvn --batch-mode --quiet install && cd /home/ubuntu/dir1806/boxfuse-sample-java-war-hello/target && aws s3 cp hello-1.0.war s3://test12062022/hello-1.0.war
+sudo git clone https://github.com/htmldav/boxfuse-sample-java-war-hello.git && cd /home/ubuntu/boxfuse-sample-java-war-hello && mvn --batch-mode --quiet install && aws s3 cp /target/hello-1.0.war s3://test12062022/hello-1.0.war
